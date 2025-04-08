238d55e417f0e17146380741f831c9a8effb0886
junhong <junhongwang31@gmail.com>
2025-04-07 20:42:33 -0400

    delete account alpha

--- home/views.py
+++ home/views.py
@@ -1,12 +1,28 @@
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
+from accounts.models import CustomUser
 from transactions.models import Transaction
-
 
 def home(request):
     transactions = Transaction.objects.all().order_by('-date')
     return render(request, 'home/home.html', {'transactions': transactions})
 
-
 def add_transaction(request):
     return render(request, 'transactions/add_transaction.html')
+
+@login_required
+def settings(request):
+    return render(request, 'home/settings.html')
+
+@login_required
+def delete_account(request):
+    user = CustomUser.objects.get(username=request.user)
+    if request.method == 'POST':
+        CustomUser.objects.get(username=user).delete()
+        logout(request)
+        return HttpResponseRedirect(reverse_lazy('home'))
+    return render(request, 'home/settings.html')
--- moneyparce/settings.py
+++ moneyparce/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -119,7 +119,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / 'moneyparce/static/',
+    os.path.join(BASE_DIR, "static"),
 ]
+
+VENV_PATH = os.path.dirname(BASE_DIR)
+STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
