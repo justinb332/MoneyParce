1a7bde8de5c9a4c33704aba19387504048ed31b8
Zoe Reed <atlzoereed@gmail.com>
2025-04-08 15:38:09 -0400

    main merge cleanup

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('transactions/', views.add_transaction, name='add_transaction'),
     path('settings/', views.settings, name='settings'),
     path('settings/delete/', views.delete_account, name='settings_delete'),
+    path('income/', views.add_income, name='add_income'),
+    path('settings/reset-data/', views.reset_data, name='reset_data'),
 ]
