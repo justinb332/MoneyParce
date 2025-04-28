09f4dd8bee07eca6a71cf0faf3d8289cf4eb8b22
junhong <junhongwang31@gmail.com>
2025-04-27 22:21:31 -0400

    merge

--- income/forms.py
+++ income/forms.py
@@ -10,15 +10,18 @@ class IncomeForm(forms.ModelForm):
             'recurrence_day_of_week', 'recurrence_day_of_month'
         ]
 
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            # Mark fields as required
-            self.fields['name'].required = True
-            self.fields['amount'].required = True  # Make this required
-            self.fields['catergory'].required = True # Ensure source is required
-            self.fields['notes'].required = False
-            self.fields['is_recurring'].required = False  # Optional field
-            self.fields['recurrence_period'].required = False  # Optional
-            self.fields['recurrence_day_of_week'].required = False  # Optional
-            self.fields['recurrence_day_of_month'].required = False  # Optional
+    def __init__(self, *args, **kwargs):
+        super(IncomeForm, self).__init__(*args, **kwargs)
+        # Mark fields as required
+        self.fields['name'].required = True
+        self.fields['amount'].required = True  # Make this required
+        self.fields['category'].required = True # Ensure source is required
+        self.fields['notes'].required = False
+        self.fields['is_recurring'].required = False  # Optional field
+        self.fields['recurrence_period'].required = False  # Optional
+        self.fields['recurrence_day_of_week'].required = False  # Optional
+        self.fields['recurrence_day_of_month'].required = False  # Optional
 
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+        self.fields['is_recurring'].widget.attrs['class'] = 'form-check-label'
